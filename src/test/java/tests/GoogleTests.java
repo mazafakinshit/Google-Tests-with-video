6e612b74b4cea638ec266726ab61d37a47b51ae7
mazafakinshit <mazafakinshit@ya.ru>
2020-06-12 00:46:50 +0300

    fourth commit added helpers and customdriver

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -1,10 +1,8 @@
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
+package tests;
+
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -18,16 +16,7 @@ import static io.qameta.allure.Allure.step;
 @Epic("QA.GURU automation course")
 @Story("Google tests with video")
 @Tag("google")
-class GoogleTests {
-
-    @BeforeEach
-    void beforeEach(){
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true));
-
-        if(System.getProperty("selenoid_url") != null) {
-            Configuration.remote = "http://" + System.getProperty("selenoid_url") + ":4444/wd/hub";
-        }
-    }
+class GoogleTests extends TestBase {
 
     @Test
     @DisplayName("успешный поиск в Google")
@@ -41,7 +30,7 @@ class GoogleTests {
             $(byName("q")).val("lepra").pressEnter();
         });
         step("Находим результат", () -> {
-            $("html").shouldHave(text("Лепра — Википедия"));});
+            $("html").shouldHave(text("Lepra"));});
 
     }
 
@@ -57,7 +46,7 @@ class GoogleTests {
             $(byName("q")).val("lepra").pressEnter();
         });
         step("Находим результат", () ->{
-            $("html").shouldHave(text("Липра — Википедия"));});
+            $("html").shouldHave(text("Leeeeepra"));});
 
     }
 }
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.CustomWebDriver;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+
+import static helpers.AttachmentsHelper.*;
+
+
+@Epic("QA.GURU automation course")
+@Story("Google tests with video")
+@Tag("google")
+class TestBase {
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true));
+        System.setProperty("selenoid_url", "selenoid.autotests.cloud");
+
+        Configuration.browser = CustomWebDriver.class.getName();
+    }
+
+    @AfterEach
+    void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachBrowserConsoleLogs();
+        attachVideo();
+    }
+}
